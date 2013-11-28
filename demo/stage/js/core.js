b11ce592700906954d66bb8853db54e50bf9322b
luofei2011 <luofeihit2010@gmail.com>
2013-11-28 19:18:57 +0800

    add stage

---
+++ demo/stage/js/core.js
@@ -0,0 +1,62 @@
+$(function() {
+	var oA = $('div.sub-header ul li a');
+	
+	// placeholder
+	FE.placeholder(document.getElementById('query-txt'));
+
+	oA.click(function() {
+		oA.each(function(i) {
+			$(this).removeClass('active');
+			$('div.item-' + (i + 1)).hide();
+		});
+		$(this).addClass('active');
+		$('div.item-' + (this.innerHTML)).show();
+
+		return false;
+	});
+});
+
+var FE = {
+	/*
+	 * @method placeholder 兼容不具备css3中placeholder属性的浏览器, 如ie < 9
+	 * @param {object} element html元素
+	 * */
+	placeholder: function(element) {
+	    var placeholder = element.getAttribute('placeholder') ||
+	                    // 兼容ie6/7
+	                      element.getAttributeNode('placeholder').nodeValue;
+
+	    if (element && !("placeholder" in document.createElement("input")) && placeholder) {
+
+	        var idLabel = element.id ;
+
+	        if (!idLabel) {
+	            idLabel = "placeholder_" + new Date().getTime();
+	            element.id = idLabel;
+	        }
+
+	        var eleLabel = document.createElement("label");
+	        eleLabel.htmlFor = idLabel;
+	        eleLabel.style.position = "absolute";
+	        eleLabel.style.marginLeft = "6px";
+	        eleLabel.style.top = "0";
+	        eleLabel.style.height = '14px';
+	        eleLabel.style.color = "gray";
+	        eleLabel.style.cursor = "text";
+	        eleLabel.style.fontSize = "13px";
+	        element.parentNode.insertBefore(eleLabel, element);
+	        element.onfocus = function() {
+	            eleLabel.innerHTML = "";
+	        };
+	        element.onblur = function() {
+	            if (this.value === "") {
+	                eleLabel.innerHTML = placeholder;  
+	            }
+	        };
+
+	        if (element.value === "") {
+	            eleLabel.innerHTML = placeholder;   
+	        }
+	    }        
+	}
+}
