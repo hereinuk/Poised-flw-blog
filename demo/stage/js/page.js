e44a0fbb601b1e7d91dc506e65cea60e4c8ac5a2
luofei2011 <luofeihit2010@gmail.com>
2013-11-29 14:33:33 +0800

    some

--- demo/stage/js/page.js
+++ demo/stage/js/page.js
@@ -6,13 +6,15 @@ $(function() {
 		left: (_w - 300) / 2,
 		top: 25,
 		"border-radius": 150
+	}, 1000).css({
+		"border-radius": '150px'
 	}).animate({
 		'width': -10,
 		'height': -10,
 		'left': _w / 2,
 		'top': '150',
 		'border-radius': $(this).width() / 2
-	}, function() {
+	}, 1000, function() {
 		$(this).remove();
 		// $('div.step-tips').slideDown('slow');
 		showStep();
@@ -28,20 +30,26 @@ $(function() {
 		$(step[0]).animate({
 			'left': 0,
 			'top': 0
-		}, 300, function() {
+		}, 600, function() {
 			// console.log(this);
+			$('div.step-to', $(this)).show().animate({
+				'width': 280
+			}, 1000);
 			$('div.step-tips', $(this)).slideDown('slow');
 		});
 		$(step[1]).animate({
 			'left': 330,
 			'top': 0,
-		}, 400, function() {
+		}, 900, function() {
+			$('div.step-to', $(this)).show().animate({
+				'width': 280
+			}, 1000);
 			$('div.step-tips', $(this)).slideDown('slow');
 		});
 		$(step[2]).animate({
 			'left': 660,
 			'top': 0
-		}, 500, function() {
+		}, 1000, function() {
 			$('div.step-tips', $(this)).slideDown('slow');
 		});
 	}
