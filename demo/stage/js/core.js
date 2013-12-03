e6fb64c2d9a19e4b5b789497f81a6bc3bcfa1a1c
luofei2011 <luofeihit2010@gmail.com>
2013-12-03 21:06:24 +0800

    update

--- demo/stage/js/core.js
+++ demo/stage/js/core.js
@@ -15,24 +15,57 @@ $(function() {
 		return false;
 	});
 
-	$('div.item').hover(function() {
-		$('div.pin-logo', $(this)).stop().show().animate({
-			'right': 10,
-			'top': 0
-		});
-		$('div.item-corner', $(this)).stop().show().animate({
-			"border-left-width": 55,
-			"border-bottom-width": 35
-		});
-	}, function() {
-		$('div.pin-logo', $(this)).stop().animate({
-			'right': -10,
-			'top': -20
-		}).hide();
-		$('div.item-corner', $(this)).stop().animate({
-			"border-left-width": 1,
-			"border-bottom-width": 1
-		}).hide();
+	// $('div.item').hover(function() {
+	// 	$('div.item-corner', $(this)).stop().show().animate({
+	// 		"border-left-width": 55,
+	// 		"border-bottom-width": 35
+	// 	});
+	// }, function() {
+	// 	$('div.item-corner', $(this)).stop().animate({
+	// 		"border-left-width": 1,
+	// 		"border-bottom-width": 1
+	// 	}).hide();
+	// });
+
+	// var _timer = setTimeout(function() {
+	// 	$('div.go-n-btn').animate({
+	// 		'margin-top': 60
+	// 	}).animate({
+	// 		'margin-top': 25
+	// 	});
+
+	// 	setTimeout(arguments.callee, 1500);
+	// }, 100);
+
+
+	$('div.go-next').on('click', function() {
+		// $('div.main-doc').slideUp('slow');
+		// $('#o-doc').slideDown('slow');
+		// window.location.href = "#o-doc";
+		// $(window).scrollTop($('.main-doc').height() + 40);
+		$("body, html").animate({
+			scrollTop: $('#o-doc').offset().top
+		}, 1000);
+	});
+
+	$(window).on('scroll', function() {
+		// console.log('scroll!!!');
+		var _d = $('body').scrollTop() || $('html').scrollTop(),
+			_t = $('#totop').parent();
+
+		if (_d >= 623 && _t.css('display') == "none") {
+			_t.show();
+		} else if (_d < 623 && _t.css('display') == "block") {
+			_t.hide();
+		}
+	});
+
+	$('#totop').on('click', function() {
+		$('body').animate({
+			scrollTop: 0
+		}, 1000);
+
+		return false;
 	});
 });
 
--- demo/stage/js/page.js
+++ demo/stage/js/page.js
@@ -17,40 +17,92 @@ $(function() {
 	}, 1000, function() {
 		$(this).remove();
 		// $('div.step-tips').slideDown('slow');
-		showStep();
+		showStep(0, 0, 3);
 	});
 
-	function showStep() {
-		var step = $('div.stepNum');
-		step.css({
-			'position': 'absolute',
-			'left': '475px',
-			'top': '100px'
-		}).show();
-		$(step[0]).animate({
-			'left': 0,
-			'top': 0
-		}, 600, function() {
-			// console.log(this);
-			$('div.step-to', $(this)).show().animate({
-				'width': 280
-			}, 1000);
-			$('div.step-tips', $(this)).slideDown('slow');
-		});
-		$(step[1]).animate({
-			'left': 330,
-			'top': 0,
-		}, 900, function() {
-			$('div.step-to', $(this)).show().animate({
-				'width': 280
-			}, 1000);
-			$('div.step-tips', $(this)).slideDown('slow');
-		});
-		$(step[2]).animate({
-			'left': 660,
-			'top': 0
-		}, 1000, function() {
-			$('div.step-tips', $(this)).slideDown('slow');
-		});
+	var _ha = [];
+	function showStep(top, idx, num) {
+		var step = $('div.stepNum'),
+			_h = 0, i, len, j = 0,// 记录高度最高的板块
+			_c = $('div.step-container'),
+			_config = [
+				{
+					top: top,
+					left: 0,
+					time: 600
+				},
+				{
+					top: top,
+					left: 330,
+					time: 900
+				}, 
+				{
+					top: top,
+					left: 660,
+					time: 1000
+				}
+			],
+			_sure = []; // 确认栈、确认所有动画效果执行完毕
+
+		if (idx === 0) {
+			step.css({
+				'position': 'absolute',
+				'left': '455px',
+				'top': '50px'
+			}).show();
+		}
+
+		for (i = idx; i < num; i++) {
+			;(function(i) {
+				$(step[i]).animate({
+					'left': _config[j].left,
+					'top': _config[j].top
+				}, _config[j].time, function() {
+					// console.log(this);
+					console.log(i, step.length);
+					if (i < step.length - 1) {
+						$('div.step-to', $(this)).show().animate({
+							'width': 280
+						}, 1000);
+					}
+					
+					$('div.step-tips', $(this)).slideDown('slow', function() {
+						_sure.push('OK');
+						var __h = $('div.step-content', $(this))/*.eq(Math.ceil(idx / 3))*/.height(),
+							_cH = _c.height(), _l,
+							_sum = count(_ha);
+
+						if (__h + _sum + 59 >= _cH) {
+							_h = __h + _sum + 59;
+							_c.height(_h);
+						} else {
+							_h = _cH;
+						}
+
+						// 说明该动画执行完毕
+						if (_sure.length === 3) {
+							_ha.push(_h + 10);
+							_sure.length = 0;
+							_l = (step.length - 3 > 3) ? 3 : step.length - 3;
+							if (_l > 0)
+								showStep(_h + 10, idx + 3, _l + idx + 3);
+						}
+					});
+				});
+			})(i);
+
+			j += 1;
+		}
+	}
+
+	function count(arr) {
+		var i = 0, len = arr.length,
+			sum = 0;
+
+		for (; i < len; i++) {
+			sum += arr[i];
+		}
+
+		return sum;
 	}
 });
